a862351175c76329d4da65298d56d2c6a87fc4d5
slk18bg <slk18bg@my.fsu.edu>
2023-11-04 10:52:42 -0700

    another push

--- src/pages/api/service/getGameNames.js
+++ src/pages/api/service/getGameNames.js
@@ -1,4 +1,5 @@
 import CallIGDB from "./callIGDB.js";
+import GetAgeRating from "./getAgeRatings.js";
 import GetGenres from "./getGenres.js";
 import GetMultiPlayerMode from "./getMultiPlayerMode.js";
 import GetPlatforms from "./getPlatforms.js";
@@ -13,37 +14,39 @@ const GetGameName = async () => {
     const platforms = await GetPlatforms();
     const multiPlayers = await GetMultiPlayerMode();
     const releaseDate = await GetReleaseDate();
+    const ageRating = await GetAgeRating();
 
     let genreIds = "(";
     let platformIds = "(";
     let multiPlayerIds = "(";
     let releaseDateIds = "(";
 
+    console.log(ageRating);
     genres.map((genre) => {
         genreIds += `${genre.id}, `;
     })
     genreIds = genreIds.slice(0, -2) + ")";
-    console.log("genres: " + genreIds);
+    //console.log("genres: " + genreIds);
 
 
     platforms.map((platform) => {
         platformIds += `${platform.id}, `;
     })
     platformIds = platformIds.slice(0, -2) + ")";
-    console.log("platforms: " + platformIds);
+    //console.log("platforms: " + platformIds);
 
     multiPlayers.map((mp) => {
         multiPlayerIds += `${mp.id}, `;
     })
     multiPlayerIds = multiPlayerIds.slice(0, -2) + ")";
-    console.log("multiplayer: " + multiPlayerIds);
+    //console.log("multiplayer: " + multiPlayerIds);
 
     // this probbly needs to be reworked; release date should be array of 2 years, min and max
     releaseDate.map((date) => {
         releaseDateIds += `${date}, `;
     })
     releaseDateIds = releaseDateIds.slice(0, -2) + ")";
-    console.log("release date: " + releaseDateIds);
+    //console.log("release date: " + releaseDateIds);
 
     const url = "https://api.igdb.com/v4/games/"
     const query = `fields name; where genres.id = ${genreIds} & platforms.id = ${platformIds} & game_modes = 1 & release_dates.y > 1993 & release_dates.y < 1995; sort release_dates.y asc; limit 10;`;
@@ -52,7 +55,7 @@ const GetGameName = async () => {
 
     const res = [...response];
     //console.log("response: " + JSON.stringify(response));
-    console.log(res);
+    //console.log(res);
 
 
 }
